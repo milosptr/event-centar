f2fb784676c2058f0e0badacb3325d39b8525f26
Evgeniya <evgeniya@ultimatemovers.net>
2021-10-12 12:13:31 +0200

    add img, style.css, main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+const navMain = document.querySelector('.menu');
+const navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', function() {
+navMain.classList.toggle('menu--opened')
+navToggle.classList.toggle('nav-toggle--close');
+}
+);
