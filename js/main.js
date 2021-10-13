44f7f75c50326f3a7e12d47e833890b1a377e7a2
Evgeniya <evgeniya@ultimatemovers.net>
2021-10-13 14:30:34 +0200

    list style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+let members = document.querySelectorAll('.team-member');
+
+members.forEach((item, index) => {
+    (index % 2 == 0) ?item.classList.add('odd'):item.classList.add('even')
+});
+
 const navMain = document.querySelector('.menu');
 const navToggle = document.querySelector('.nav-toggle');
 
