ce493d6f952067d5a310bca26a837c6544f07715
Vladimir <vladimir96ks@gmail.com>
2021-11-30 17:16:50 +0100

    smooth scroll added

--- js/main.js
+++ js/main.js
@@ -20,6 +20,79 @@
     });
   }
 
+
+  function init(){
+    // new SmoothScroll(target,speed,smoothe)
+    new SmoothScroll(document,50,16)
+  }
+  
+  function SmoothScroll(target, speed, smooth) {
+    if (target === document)
+      target = (document.scrollingElement 
+                || document.documentElement 
+                || document.body.parentNode 
+                || document.body) 
+        
+    var moving = false
+    var pos = target.scrollTop
+    var frame = target === document.body 
+                && document.documentElement 
+                ? document.documentElement 
+                : target
+    
+    target.addEventListener('mousewheel', scrolled, { passive: false })
+    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+  
+    function scrolled(e) {
+      e.preventDefault();
+  
+      var delta = normalizeWheelDelta(e)
+  
+      pos += -delta * speed
+      pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight))
+  
+      if (!moving) update()
+    }
+  
+    function normalizeWheelDelta(e){
+      if(e.detail){
+        if(e.wheelDelta)
+          return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
+        else
+          return -e.detail/3 // Firefox
+      }else
+        return e.wheelDelta/120 // IE,Safari,Chrome
+    }
+  
+    function update() {
+      moving = true
+      
+      var delta = (pos - target.scrollTop) / smooth
+      
+      target.scrollTop += delta
+      
+      if (Math.abs(delta) > 0.5)
+        requestFrame(update)
+      else
+        moving = false
+    }
+  
+    var requestFrame = function() { // requestAnimationFrame cross browser
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(func) {
+          window.setTimeout(func, 1000 / 50);
+        }
+      );
+    }()
+  }
+
+
+
   // if(document.getElementById('about-broomley'))
   //   new Vara("#about-broomley","/fonts/font.json",[{
   //     text: "Bromley",
@@ -303,4 +376,6 @@
   if(document.querySelector('.reveal-top-delay'))
     ScrollReveal().reveal('.reveal-top-delay', { delay: 300, distance: '30px', duration: 700, origin: 'bottom', easing: 'cubic-bezier(0.3,.62,1,1)' });
 
+
+  document.body.addEventListener('onload', init())
 })()
